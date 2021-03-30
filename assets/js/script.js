f793355e6b043d087586ed0d1a1ba081011f6fe4
Lucre <josielindamaldonado@gmail.com>
2021-03-30 00:06:57 -0400

    put files in foler worked on html hard coding. started api function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,33 @@
-console.log("Hello World")
+var searchBar = $("#search-bar");
+var searchTermEl = $("#search-term");
+var apiKey = "ee576a8bcd80830440ad2e16c65599ee" 
+
+$(document).ready($(".searchBtn").on("click", function(event) {
+    event.preventDefault();
+    var searchTerm = searchTermEl.val();
+    console.log(searchTerm);
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&appid=" + apiKey;
+    console.log(queryURL);
+    fetch(queryURL).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        console.log(data);
+    })
+}));
+
+
+
+
+
+
+
+
+
+
+
+
+// searchBar.on("click", function(event) {
+//     event.preventDefault();
+//     var searchTerm = searchTermEl.val();
+//     console.log(searchTerm);
+// });
