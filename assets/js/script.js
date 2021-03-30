789136b23f1b777e2d9232d529ed64efcaa9b8e8
Lucre <josielindamaldonado@gmail.com>
2021-03-30 10:14:05 -0400

    fixing html to display date and city.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,21 @@ var searchTermEl = $("#search-term");
 var apiKey = "d84e1509129ac5ce8200b78e8284de7b" 
 
 $(document).ready($(".searchBtn").on("click", function(event) {
+    var currentDate = moment().format("L");
+    console.log(currentDate);
+    var displayDate = $("<h2>").text(currentDate);
     event.preventDefault();
     var searchTerm = searchTermEl.val();
     console.log(searchTerm);
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&appid=" + apiKey + "&units=imperial";
+    
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&appid=" + apiKey + "&units=imperial";
     console.log(queryURL);
     fetch(queryURL).then(function (response) {
         return response.json();
     }).then(function (data) {
+        var cityDateDisplay = data.name + " " + displayDate;
+        console.log(cityDateDisplay);
+        $(".city-date").append(cityDateDisplay);
         console.log(data);
         console.log(data.name);
         console.log(data.main.temp);
@@ -18,7 +25,7 @@ $(document).ready($(".searchBtn").on("click", function(event) {
         console.log(data.wind.speed);
         console.log(data.coord.lon);
         console.log(data.coord.lat);
-        var uvIndexURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + {lat} + "&lon=" + {lon} + "&appid=" + apiKey
+        // var uvIndexURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + {lat} + "&lon=" + {lon} + "&appid=" + apiKey
     })
 }));
 
