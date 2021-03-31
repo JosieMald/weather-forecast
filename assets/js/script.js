d5ee1280736094482bc8bc4b86d7f00b37003520
Lucre <josielindamaldonado@gmail.com>
2021-03-30 23:34:02 -0400

    Finished readme.

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,21 +9,23 @@ $(document).ready(function(event) {
     }
     var currentDate = moment().format("L");
     console.log(currentDate);
-    // var displayDate = $("<h2>").text(currentDate);
-    // event.preventDefault();
+    
+    // Click Function
     $(".searchBtn").on("click", function (){
+        // Stores user input 
         var searchTerm = searchTermEl.val();
-        // console.log(searchTerm);
         searchWeather(searchTerm);
         history.push(searchTerm);
         console.log(history);
-        localStorage.setItem("history", JSON.stringify(history))
+        localStorage.setItem("history", JSON.stringify(history));
     })
+    //Creates history list of previously searched cities
     function createMenuItem(text){
         var li = $("<li>").text(text).addClass("list-group-item");
         $(".list-group").append(li);
     };
 
+    // Fecthes data from first API
     function searchWeather(searchTerm) {
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&appid=" + apiKey + "&units=imperial";
     console.log(queryURL);
@@ -31,10 +33,10 @@ $(document).ready(function(event) {
     fetch(queryURL).then(function (response) {
         return response.json();
     }).then(function (data) {
-        var cityDateDisplay = data.name + " " + currentDate; // was displayDate but kept logging object 
-        var temp = $("<p>").text("Temperature: " + data.main.temp);
-        var humidity = $("<p>").text("Humidity: " + data.main.humidity);
-        var windSpeed = $("<p>").text("Wind Speed: " + data.wind.speed);
+        var cityDateDisplay = data.name + " " + currentDate; 
+        var temp = $("<p>").text("Temperature: " + data.main.temp + " °F");
+        var humidity = $("<p>").text("Humidity: " + data.main.humidity + " %");
+        var windSpeed = $("<p>").text("Wind Speed: " + data.wind.speed + " MPH");
         $(".city-date").append(cityDateDisplay);
         $(".temperature").append(temp);
         $(".humidity").append(humidity);
@@ -44,7 +46,9 @@ $(document).ready(function(event) {
         // console.log(data.coord.lat);
         getForecast(data.coord.lat, data.coord.lon);
     })
-}
+};
+
+// Second fetch API for UV Index info and daily weather stats
 function getForecast(lat,lon){
     var queryURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely&units=imperial&appid=${apiKey}`
     fetch(queryURL).then(function (response) {
@@ -56,29 +60,12 @@ function getForecast(lat,lon){
         $(".uv-index").append(uvIndex);
         for( var i = 1; i < 6; i++){
             console.log(data.daily[i]);
-            var card = $("<div>").addClass("card").attr("style", "border: 1px solid black")
+            var card = $("<div>").addClass("card").attr("style", "border: 1px solid black");
             var cardBody = $("<div>").addClass("card-body");
-            var cardTitle = $("<h5>").addClass("card-title").text(moment.unix(data.daily[i].dt).format("dddd"));
+            var cardTitle = $("<h5>").addClass("card-title").text(moment.unix(data.daily[i].dt).format("L"));
             
             $(".forecast").append(card.append(cardBody.append(cardTitle)));
         }
 })
 }
 });
-
-
-// $(".searchBtn").on("click",
-
-
-
-
-
-
-
-
-
-// searchBar.on("click", function(event) {
-//     event.preventDefault();
-//     var searchTerm = searchTermEl.val();
-//     console.log(searchTerm);
-// });
